24921afeb6da63219e251791d7bf581b2c682063
kozak.honza <kozak.honza@gmail.com>
2015-11-03 23:26:39 +0100

    NEW: pridany modely Course, Lesson, Slide, Component, ComponentConfig

---
+++ courses/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course)
+    name = models.CharField(max_length=30)
+    order = models.IntegerField()
+
+
+class Slide(models.Model):
+    lesson = models.ForeignKey(Lesson)
+    order = models.IntegerField()
+
+
+class Component(models.Model):
+    TYPES = (
+        (1, 'Text'),
+        (1, 'Image'),
+    )
+    parent = models.ForeignKey('self')
+    type = models.IntegerField(choices=TYPES)
+    order = models.IntegerField()
+
+
+class CompoenentConfig(models.Model):
+    component = models.ForeignKey(Component)
+    key = models.IntegerField()
+    value = models.CharField(max_length=254)
