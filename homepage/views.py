7b432b567bad8915ce6422c6e3a8126d81f93754
kozak.honza <kozak.honza@gmail.com>
2015-11-04 23:32:34 +0100

    NEW: pridan boostrap

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from homepage.forms import NameForm
 
 
 def index(request):
-    return render(request, 'homepage/index.html')
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = NameForm()
+        return render(request, 'homepage/index.html', {'form': form})
