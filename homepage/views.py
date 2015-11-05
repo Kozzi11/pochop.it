8fddce304d4c011cef92b6436a05d8fda864e446
kozakj <kozak.honza@gmail.com>
2015-11-05 15:55:34 +0100

    NEW: pridano menu

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,11 @@
+from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from homepage.forms import NameForm
 
 
 def index(request):
+    _("test prekladu")
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
