b14630bbeb69916059066cf3d25b6c2882f9826a
kozak.honza <kozak.honza@gmail.com>
2015-11-12 22:36:54 +0100

    NEW: stranka pro kurzy

--- _courses/views.py
+++ _courses/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def courses(request, tab_name):
-    return render(request, '_courses/courses.html', {'tab_name': tab_name})
+    tabs = {
+        'in_progress': tab_name == 'in_progress',
+        'all': tab_name == 'all',
+        'completed': tab_name == 'completed',
+        'my_courses': tab_name == 'my_courses',
+    }
+    template_name = '_courses/' + tab_name + '.html'
+    return render(request, template_name, {'tabs': tabs})
