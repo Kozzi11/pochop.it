a5ff6ec83b2a5d7584b079e922cb4605d183e4d3
kozakj <kozak.honza@gmail.com>
2015-11-16 16:43:36 +0100

    INFO: pridany informace jak si rozchodit eztables(grid)

--- _courses/views.py
+++ _courses/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
 from django.contrib.auth import get_user
-from django_tables2 import RequestConfig
 from _courses.models import Course
-from _courses.tables import CourseTable
 from eztables.views import DatatablesView
-from pochopit.viewcomponents.tab import Tab
+# from pochopit.viewcomponents.tab import Tab
 
 
 def courses(request, tab_name):
@@ -15,17 +13,14 @@ def courses(request, tab_name):
         tab_name = 'in_progress'
 
     tabs = [
-            Tab(_('All'), reverse('courses', args=('all',)), tab_name == 'all'),
-            Tab(_('In progress'), reverse('courses', args=('in_progress',)), tab_name == 'in_progress', 2),
-            Tab(_('Completed'), reverse('courses', args=('completed',)), tab_name == 'completed', 8),
-            Tab(_('My courses'), reverse('courses', args=('my',)), tab_name == 'my', 3)
+            # Tab(_('All'), reverse('courses', args=('all',)), tab_name == 'all'),
+            # Tab(_('In progress'), reverse('courses', args=('in_progress',)), tab_name == 'in_progress', 2),
+            # Tab(_('Completed'), reverse('courses', args=('completed',)), tab_name == 'completed', 8),
+            # Tab(_('My courses'), reverse('courses', args=('my',)), tab_name == 'my', 3)
     ]
 
     template_name = '_courses/' + tab_name + '.html'
-
-    table = CourseTable(Course.objects.all())
-    RequestConfig(request).configure(table)
-    return render(request, template_name, {'tabs': tabs, 'courses': table})
+    return render(request, template_name, {'tabs': tabs})
 
 
 def new(request):
