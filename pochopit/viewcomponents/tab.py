1200f44b45cbc7ca9bcde02dedc0e45302f55113
kozak.honza <kozak.honza@gmail.com>
2015-11-18 21:27:39 +0100

    tabs

--- pochopit/viewcomponents/tab.py
+++ pochopit/viewcomponents/tab.py
@@ -1,1 +1,18 @@
-__author__ = 'kozak_000'
+
+
+class Tab:
+    name = ''
+    url = '#'
+    className = ''
+    addBaddge = False
+    baddgeNumber = 0
+
+    def __init__(self, name: str, url: str, is_active: bool, buddge_number=None):
+        self.name = name
+        self.url = url
+        if is_active:
+            self.className = 'active'
+
+        if buddge_number is not None:
+            self.baddgeNumber = buddge_number
+            self.addBaddge = True
