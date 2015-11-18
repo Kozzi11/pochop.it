edadb63af8649f61f760f4afca4a3a3062928aa3
kozak.honza <kozak.honza@gmail.com>
2015-11-18 23:02:24 +0100

    NEW: taby predelany na ajax

--- _courses/urls.py
+++ _courses/urls.py
@@ -4,8 +4,15 @@ from . import views
 from _courses.views import CourseDatatablesView
 
 urlpatterns = [
-    url(r'^courses/kokos$', CourseDatatablesView.as_view(),  name='course_datatables_view'),
+    url(r'^courses/data', CourseDatatablesView.as_view(),  name='course_datatables_view'),
     url(r'^courses/new$', views.new, name='courses_new'),
     url(r'^courses/edit/(.*)$', views.edit, name='courses_edit'),
-    url(r'^courses/(.*)$', views.courses, name='courses'),
+    url(r'^courses/edit-main-tab/(.*)$', views.edit_main_tab, name='courses_edit_main_tab'),
+    url(r'^courses/edit-lessons-tab/(.*)$', views.edit_lessons_tab, name='courses_edit_lessons_tab'),
+    url(r'^courses/all$', views.all_tab, name='courses_all'),
+    url(r'^courses/in_progress$', views.in_progress_tab, name='courses_in_progress'),
+    url(r'^courses/completed$', views.completed_tab, name='courses_completed'),
+    url(r'^courses/my$', views.my_tab, name='courses_my'),
+    url(r'^courses', views.courses, name='courses'),
+
 ]
--- _courses/views.py
+++ _courses/views.py
@@ -8,19 +8,30 @@ from eztables.views import DatatablesView
 from pochopit.viewcomponents.tab import Tab
 
 
-def courses(request, tab_name):
-    if not tab_name:
-        tab_name = 'in_progress'
-
+def courses(request):
     tabs = [
-            Tab(_('All'), reverse('courses', args=('all',)), tab_name == 'all'),
-            Tab(_('In progress'), reverse('courses', args=('in_progress',)), tab_name == 'in_progress', 2),
-            Tab(_('Completed'), reverse('courses', args=('completed',)), tab_name == 'completed', 8),
-            Tab(_('My courses'), reverse('courses', args=('my',)), tab_name == 'my', 3)
+            Tab(_('All'), reverse('courses_all'), True),
+            Tab(_('In progress'), reverse('courses_in_progress'), False, 2),
+            Tab(_('Completed'), reverse('courses_completed'), False, 8),
+            Tab(_('My courses'), reverse('courses_my'), False, 3)
     ]
+    return render(request, 'pochopit/base_side_panel.html', {'tabs': tabs})
 
-    template_name = '_courses/' + tab_name + '.html'
-    return render(request, template_name, {'tabs': tabs})
+
+def all_tab(request):
+    return render(request, '_courses/all.html')
+
+
+def in_progress_tab(request):
+    return render(request, '_courses/in_progress.html')
+
+
+def completed_tab(request):
+    return render(request, '_courses/completed.html')
+
+
+def my_tab(request):
+    return render(request, '_courses/my.html')
 
 
 def new(request):
@@ -30,8 +41,20 @@ def new(request):
     return HttpResponseRedirect(reverse('courses_edit', args=(course.id,)))
 
 
-def edit(request, id):
-    return HttpResponse("test")
+def edit(request, course_id):
+    tabs = [
+        Tab(_('Main'), reverse('courses_edit_main_tab', args=(course_id,)), True),
+        Tab(_('Lessons'), reverse('courses_edit_lessons_tab', args=(course_id,)), False),
+    ]
+    return render(request, 'pochopit/base_side_panel.html', {'tabs': tabs})
+
+
+def edit_main_tab(request, course_id):
+    return render(request, '_courses/edit/main.html')
+
+
+def edit_lessons_tab(request, course_id):
+    return render(request, '_courses/edit/lessons.html')
 
 
 class CourseDatatablesView(DatatablesView):
