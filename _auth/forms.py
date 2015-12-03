299c7173d2e589c7056b2dcfb8e473baad39e863
ofeig <tomashegru@gmail.com>
2015-12-03 21:29:32 +0100

    registracni formular - prvni commit

--- _auth/forms.py
+++ _auth/forms.py
@@ -1,8 +1,25 @@
 from django.utils.translation import ugettext as _
 from django import forms
+from django.contrib.auth.models import User
 
 
 class SignUpForm(forms.Form):
     first_name = forms.CharField(label=_('First name'), max_length=100)
     last_name = forms.CharField(label=_('Last name'), max_length=100)
     age = forms.IntegerField(label=_('Age'))
+    email = forms.EmailField(label=_('E-mail'), max_length=254, required=True)
+    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput, required=True)
+    password_confirm = forms.CharField(label=_('Confirm password'), widget=forms.PasswordInput, required=True)
+
+    def unique_user(self):
+        try:
+            user = User.objects.get(username__iexact=self.cleaned_data['email'])
+        except User.DoesNotExist:
+            return self.cleaned_data['email']
+        raise forms.ValidationError(_("This email is already registered. If you've forgotten your password, generate a new one."))
+
+    def confirm_password(self):
+        if 'password' in self.cleaned_data and 'password_confirm' in self.cleaned_data:
+            if self.cleaned_data['password'] != self.cleaned_data['password_confirm']:
+                raise forms.ValidationError(_("The two password fields did not match!"))
+        return self.cleaned_data
--- _auth/views.py
+++ _auth/views.py
@@ -1,13 +1,32 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from _auth.forms import SignUpForm
+from django.contrib.auth.models import User
 
 
 def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('_auth/thanks.html')
     else:
         form = SignUpForm()
         return render(request, '_auth/sign_up.html', {'form': form})
+
+def registration(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+                email=form.cleaned_data['email'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name']
+            )
+            return HttpResponseRedirect('/sing_up/success')
+        else:
+            form = SignUpForm()
+            return render('_auth{sing_up.html', {'form': form})
+
+
