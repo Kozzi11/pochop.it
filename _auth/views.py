08cf205d1f2c18bd8cc6476e1233eabc71556dac
ofeig <tomashegru@gmail.com>
2015-12-08 15:43:50 +0100

    sys: oprava registrace uzivatelu

--- _auth/views.py
+++ _auth/views.py
@@ -1,32 +1,25 @@
+from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from _auth.forms import SignUpForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
 
 
 def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('_auth/thanks.html')
-    else:
-        form = SignUpForm()
-        return render(request, '_auth/sign_up.html', {'form': form})
-
-def registration(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
             user = User.objects.create_user(
-                username=form.cleaned_data['email'],
+                form.cleaned_data['email'],
                 password=form.cleaned_data['password'],
-                email=form.cleaned_data['email'],
-                first_name=form.cleaned_data['first_name'],
-                last_name=form.cleaned_data['last_name']
-            )
+                email=form.cleaned_data['email'])
+            user = auth.authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            auth.login(request, user)
             return HttpResponseRedirect('/sing_up/success')
         else:
             form = SignUpForm()
-            return render('_auth{sing_up.html', {'form': form})
+            return render(request, '_auth/sign_up.html', {'form': form})
 
+    form = SignUpForm()
+    return render(request, '_auth/sign_up.html', {'form': form})
 
